5a36937a22893727bdae257c2dc1abf2bf8196e1
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2017-08-09 14:05:43 +0800

    Add files via upload

---
+++ IOCC_normalize.bat
@@ -0,0 +1,17 @@
+netsh interface ip set address "Local Area Connection" dhcp
+
+netsh interface ip set dnsservers "Local Area Connection" dhcp
+
+netsh interface ip set address "Wireless Network Connection 19" dhcp
+
+netsh interface ip set dnsservers "Wireless Network Connection 19" dhcp
+
+
+@pause
+
+ipconfig
+
+pause
+
+
+
