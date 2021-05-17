f7bbb6dad7c23195f30a5d41b191fa59e6d8d50d
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2021-05-17 23:57:42 +0800

    Add files via upload

---
+++ Launch_PSR.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+
+rem set today=%date:~10,4%%date:~4,2%%date:~7,2%
+rem set today=%date:~7,2%-%date:~4,2%-%date:~10,4%_%time:~0,2%_%time:~3,2%_%time:~6,2%
+set today=PSR-%date:~10,4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%%time:~6,2%
+
+echo Today date =%today%
+
+psr.exe /start /output C:\Users\P1206812\Documents\%today%.zip /sc 1 /maxsc 99 /stopevent 123456
+psr.exe /stop
