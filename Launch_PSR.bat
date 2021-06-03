5f4debbacd6430243b59428351a42750a8df5342
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2021-06-03 16:07:29 +0800

    Update Launch_PSR.bat
    DTS format was yyyyddmm  Corrected to yyyymmdd

--- Launch_PSR.bat
+++ Launch_PSR.bat
@@ -3,7 +3,7 @@ cls
 
 rem set today=%date:~10,4%%date:~4,2%%date:~7,2%
 rem set today=%date:~7,2%-%date:~4,2%-%date:~10,4%_%time:~0,2%_%time:~3,2%_%time:~6,2%
-set today=PSR-%date:~10,4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%%time:~6,2%
+set today=PSR-%date:~10,4%%date:~7,2%%date:~4,2%%time:~0,2%%time:~3,2%%time:~6,2%
 
 echo Today date =%today% and start Steps Recorder
 
