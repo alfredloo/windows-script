dd97480cc77f7d2f5fc2d1e0cff941139f556663
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2021-06-05 11:16:46 +0800

    Update Launch_PSR.bat

--- Launch_PSR.bat
+++ Launch_PSR.bat
@@ -6,6 +6,10 @@ rem set today=%date:~7,2%-%date:~4,2%-%date:~10,4%_%time:~0,2%_%time:~3,2%_%time
 set today=PSR-%date:~10,4%%date:~7,2%%date:~4,2%%time:~0,2%%time:~3,2%%time:~6,2%
 
 echo Today date =%today% and start Steps Recorder
+echo 
+echo "WARNING !!!!"
+echo "Do not pause and start the Recorder or data will be lost"
+echo "Always exit and launch again"
 
-psr.exe /start /output C:\Users\P1206812\Documents\%today%.zip /sc 1 /maxsc 99 /stopevent 123456
+psr.exe /start /output C:\Users\<userid>\Documents\%today%.zip /sc 1 /maxsc 99 /stopevent 123456
 psr.exe /stop
