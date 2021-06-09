87e1f1482ed97e014f1cb8a6a57023cf305d8cbe
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2021-06-09 11:38:57 +0800

    Update Launch_PSR.bat

--- Launch_PSR.bat
+++ Launch_PSR.bat
@@ -3,8 +3,11 @@ cls
 
 rem set today=%date:~10,4%%date:~4,2%%date:~7,2%
 rem set today=%date:~7,2%-%date:~4,2%-%date:~10,4%_%time:~0,2%_%time:~3,2%_%time:~6,2%
-set today=PSR-%date:~10,4%%date:~7,2%%date:~4,2%%time:~0,2%%time:~3,2%%time:~6,2%
+rem set today=PSR-%date:~10,4%%date:~7,2%%date:~4,2%%time:~0,2%%time:~3,2%%time:~6,2%
+set hh=%time:~0,2%
+if "%time:~0,1%"==" " set hh=0%hh:~1,1%   
 
+set today=PSR-%date:~10,4%%date:~7,2%%date:~4,2%_%hh%_%time:~3,2%_%time:~6,2%
 echo Today date =%today% and start Steps Recorder
 echo 
 echo "WARNING !!!!"
@@ -13,3 +16,4 @@ echo "Always exit and launch again"
 
 psr.exe /start /output C:\Users\<userid>\Documents\%today%.zip /sc 1 /maxsc 99 /stopevent 123456
 psr.exe /stop
+pause
